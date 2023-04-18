a11e49a3d43c102a99f62fcc061aa46eb363d874
Thomas ROBERT <thomas_3004@hotmail.fr>
2023-04-18 13:52:08 +0200

    Setup scroll links

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-const menuIcon = document.querySelector('.nav-toggle');
-const navLinks = document.querySelector('.nav-links');
+const navBtn = document.querySelector('.nav-toggle');
+const links = document.querySelector('.nav-links');
+const scrollLinks = document.querySelectorAll('.nav-link');
 const date = document.querySelector('#date');
+const header = document.querySelector('#header');
+const headerHeight = header.getBoundingClientRect().height;
 
 date.textContent = new Date().getFullYear();
 
-menuIcon.addEventListener('click', () => {
-  navLinks.classList.toggle('show-links');
+navBtn.addEventListener('click', () => {
+  links.classList.toggle('show-links');
+});
+
+scrollLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const id = link.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+    let position = element.offsetTop - headerHeight;
+
+    links.classList.remove('show-links');
+    window.scrollTo({
+      left: 0,
+      top: position
+    });
+  });
 });
