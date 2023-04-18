b83b50312ee9b8b5934a7abd51e78b429a158ab0
Thomas ROBERT <thomas_3004@hotmail.fr>
2023-04-18 13:29:43 +0200

    Add dynamic date

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const menuIcon = document.querySelector('.nav-toggle');
 const navLinks = document.querySelector('.nav-links');
+const date = document.querySelector('#date');
+
+date.textContent = new Date().getFullYear();
 
 menuIcon.addEventListener('click', () => {
   navLinks.classList.toggle('show-links');
