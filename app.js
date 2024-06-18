5746fa1f6a59ae7759dc3e9c82f0435b5f6e3e78
Thomas ROBERT <thomas_3004@hotmail.fr>
2024-06-18 10:03:15 +0200

    refactor: Improve mobile navbar toggle height and dropdown behavior

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const headerHeight = header.getBoundingClientRect().height;
 const navBtn = document.querySelector('.nav-toggle');
 const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.nav-links');
-const linksHeight = links.getBoundingClientRect().height;
 const scrollLinks = document.querySelectorAll('.nav-link');
 const date = document.querySelector('#date');
 
 date.textContent = new Date().getFullYear();
 
 navBtn.addEventListener('click', () => {
-  const linksContainerHeight = linksContainer.getBoundingClientRect().height;
+  const linksContainerHeight =
+    linksContainer.getBoundingClientRect().height;
+
+  const linksHeight = links.getBoundingClientRect().height;
+  console.log(linksHeight);
 
   if (linksContainerHeight === 0) {
     linksContainer.style.height = `${linksHeight}px`;
@@ -25,11 +28,12 @@ scrollLinks.forEach((link) => {
     const id = link.getAttribute('href').slice(1);
     const element = document.getElementById(id);
     let position = element.offsetTop - headerHeight;
+    linksContainer.style.height = 0;
 
     links.classList.remove('show-links');
     window.scrollTo({
       left: 0,
-      top: position
+      top: position,
     });
   });
 });
